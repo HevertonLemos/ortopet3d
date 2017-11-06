db931603e37644700f09e18293943d795868c714
hevertonlemos <v3rton@hotmail.com>
2017-11-06 00:12:19 -0200

    correction

--- ortopet3d/settings.py
+++ ortopet3d/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'yo=2+-f1y*znz1h*3q)h8+nl9p*s4en*=cxf=^9iiujs(x#l4v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['http://ortopet3d.pythonanywhere.com/', 'ortopet3d.pythonanywhere.com']
-ALLOWED_HOSTS = ['127.0.0.1', 'admin.pythonanywhere.com']
+ALLOWED_HOSTS = ['http://ortopet3d.pythonanywhere.com/', 'ortopet3d.pythonanywhere.com']
+# ALLOWED_HOSTS = ['127.0.0.1', 'admin.pythonanywhere.com']
 
 
 # Application definition
